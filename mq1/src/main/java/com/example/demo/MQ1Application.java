19a53e0b49d7796744bb8340a395e3688b91b3a5
lizuodu <lizuodu@163.com>
2018-11-27 12:49:49 +0800

    修改名称

---
+++ mq1/src/main/java/com/example/demo/MQ1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 订单
+ * @author lizuodu
+ * @date   2018年10月27日
+ */
+@SpringBootApplication
+public class MQ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MQ1Application.class, args);
+	}
+}
